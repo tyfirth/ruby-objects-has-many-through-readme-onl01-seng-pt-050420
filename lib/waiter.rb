8f6069ca0958771b93f786560a2ac128ed71774f
Tyler Firth <tyfirth@gmail.com>
2020-06-08 02:12:58 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -26,7 +26,7 @@ class Waiter
   
   def best_tipper
     best_tipped_meal = meal.max do |meal_a, meal_b|
-      meal_a.tip <=> meal_b
+      meal_a.tip <=> meal_b.tip
     end
     
     best_tipped_meal.customer
